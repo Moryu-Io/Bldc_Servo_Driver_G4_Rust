80b09aacc0950de6775be84ae83febad33d6e4de
Moryu <moryu999@gmail.com>
2021-12-04 15:31:37 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+//use cortex_m::asm;
+use cortex_m_rt::entry;
+use stm32g4::stm32g431;
+
+#[entry]
+fn main() -> ! {
+    //asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    let peripherals = stm32g431::Peripherals::take().unwrap();
+
+    peripherals.RCC.ahb2enr.modify(|_,w| w.gpioben().set_bit());
+    let gpiob = &peripherals.GPIOB;
+    unsafe{
+        gpiob.moder.modify(|_,w| w.moder9().bits(1u8));
+    }
+    gpiob.odr.modify(|_, w| w.odr9().set_bit());
+
+    loop {
+        // your code goes here
+        
+    }
+}
